35ed4c5dbebfa60ee638d195510d15e7ab97bedb
Sage Weil <sage@redhat.com>
2017-05-16 12:44:54 -0400

    mon: move mgr report handling into separate MgrStatMonitor
    We don't want this is MgrMonitor or else the MgrMap epoch will roll over
    every few seconds when we get a mgr report.  Also, it's nice to keep this
    service separate from the unrelated MgrMap management.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -23,7 +23,7 @@
 #include "Monitor.h"
 #include "MDSMonitor.h"
 #include "PGMonitor.h"
-#include "MgrMonitor.h"
+#include "MgrStatMonitor.h"
 
 #include "MonitorDBStore.h"
 #include "Session.h"
@@ -328,8 +328,10 @@ void OSDMonitor::update_from_paxos(bool *need_bootstrap)
 
   // make sure we're using the right pg service.. remove me post-luminous!
   if (osdmap.test_flag(CEPH_OSDMAP_REQUIRE_LUMINOUS)) {
-    mon->pgservice = mon->mgrmon()->get_pg_stat_service();
+    dout(10) << __func__ << " pgservice is mgrstat" << dendl;
+    mon->pgservice = mon->mgrstatmon()->get_pg_stat_service();
   } else {
+    dout(10) << __func__ << " pgservice is pg" << dendl;
     mon->pgservice = mon->pgmon()->get_pg_stat_service();
   }
 
@@ -396,8 +398,10 @@ void OSDMonitor::update_from_paxos(bool *need_bootstrap)
 
     // make sure we're using the right pg service.. remove me post-luminous!
     if (osdmap.test_flag(CEPH_OSDMAP_REQUIRE_LUMINOUS)) {
-      mon->pgservice = mon->mgrmon()->get_pg_stat_service();
+      dout(10) << __func__ << " pgservice is mgrstat" << dendl;
+      mon->pgservice = mon->mgrstatmon()->get_pg_stat_service();
     } else {
+      dout(10) << __func__ << " pgservice is pg" << dendl;
       mon->pgservice = mon->pgmon()->get_pg_stat_service();
     }
 
