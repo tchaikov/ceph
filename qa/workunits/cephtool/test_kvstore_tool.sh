1ef54ee4c8eb08f69d4844bb7e78cc092939bd58
Kefu Chai <kchai@redhat.com>
2018-05-25 18:07:26 +0800

    wip
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- qa/workunits/cephtool/test_kvstore_tool.sh
+++ qa/workunits/cephtool/test_kvstore_tool.sh
@@ -25,8 +25,9 @@ TEMP_FILE=$(mktemp $TEMP_DIR/test_invalid.XXX)
 
 function test_ceph_kvstore_tool()
 {
-  # create a data directory
-  ceph-objectstore-tool --data-path ${TEMP_DIR} --op mkfs --no-mon-config
+    # create a data directory
+    echo "=========== test_ceph_kvstore_tool ========"
+  ceph-objectstore-tool --data-path ${TEMP_DIR} --op mkfs --no-mon-config || sleep 3h
 
   # list
   origin_kv_nums=`ceph-kvstore-tool  bluestore-kv ${TEMP_DIR} list 2>/dev/null | wc -l`
