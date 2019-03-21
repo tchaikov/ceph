67b05e7e6248c67bb2e38fc1337e326f769a7a4d
Roman Penyaev <rpenyaev@suse.de>
2019-03-21 18:10:11 +0100

    os/bluestore: introduce new io_uring IO engine
    This implements low-level IO engine, which utilizes brand-new
    io_uring IO interface: https://lwn.net/Articles/776428/
    
    By default libaio is used.  If bluestore_ioring=true is set but kernel
    does not support io_uring or architecture is not x86-64, libaio will be
    used instead.
    
    In current patch liburing library is used in order not to open code
    everything.
    
    In order to compile with liburing WITH_LIBURING=ON should be specified.
    
    Signed-off-by: Roman Penyaev <rpenyaev@suse.de>

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -93,6 +93,9 @@
 /* Defined if you have libaio */
 #cmakedefine HAVE_LIBAIO
 
+/* Defined if you have liburing */
+#cmakedefine HAVE_LIBURING
+
 /* Defind if you have POSIX AIO */
 #cmakedefine HAVE_POSIXAIO
 
