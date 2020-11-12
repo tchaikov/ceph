5f77e85c55d8b8ff41a916b1d0d1c332d6fe22c7
Kefu Chai <kchai@redhat.com>
2020-11-12 16:58:12 +0800

    crimson-bt
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/crimson/test_errorator.cc
+++ src/test/crimson/test_errorator.cc
@@ -1,8 +1,11 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
 // vim: ts=8 sw=2 smarttab
 
+#include <csignal>
+#include <seastar/apps/lib/stop_signal.hh>
 #include "test/crimson/gtest_seastar.h"
 
+#include "crimson/common/fatal_signal.h"
 #include "crimson/common/errorator.h"
 #include "crimson/common/log.h"
 
@@ -12,6 +15,7 @@ struct do_until_test_t : public seastar_test_suite_t {
     return crimson::do_until([this] {
       if (i < 5) {
         ++i;
+        std::raise(SIGSEGV);
         return ertr::make_ready_future<bool>(false);
       } else {
         return ertr::make_ready_future<bool>(true);
@@ -25,6 +29,9 @@ struct do_until_test_t : public seastar_test_suite_t {
 TEST_F(do_until_test_t, basic)
 {
   run_async([this] {
+    seastar_apps_lib::stop_signal stop_signal;
+    FatalSignal fatal_signal;
     test().unsafe_get0();
+    stop_signal.wait().get();
   });
 }
